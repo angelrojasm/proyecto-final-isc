532c056cb81bc966aac2ee3f3c8456b214bbed80
angelrojasm <angelrojasm6@gmail.com>
2021-07-17 22:18:45 -0400

    Moved ML Model code to Main project repository

---
+++ models/server/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import request
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+import joblib
+
+
+clasifier = joblib.load('./classifier.joblib')
+vectorizer = joblib.load('./vectorizer.joblib')
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    request_data = request.get_json()
+    vectorizedMessage = vectorizer.transform([request_data['message']])
+    return clasifier.predict(vectorizedMessage)[0]
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4001)
