a0cd8a8a32c148422abadd822e88575fd9afd0d5
cocacolaboxers <jtomasdiaz.98@icloud.com>
2021-07-26 14:47:12 -0400

    transition

--- app/src/screens/ProfileSetup.tsx
+++ app/src/screens/ProfileSetup.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {  Text, View, StyleSheet, TouchableOpacity, TextInput, Button, FlatList } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
 import { StackScreenProps } from '@react-navigation/stack';
-import { AuthStackParamList} from '../navigation/types.navigation';
+import {RootStackParamList } from '../navigation/types.navigation';
 import { useState } from 'react';
 import RNPickerSelect from 'react-native-picker-select';
 
-
-
-const ProfileSetup = () => {
+const ProfileSetup = ({ navigation }: StackScreenProps<RootStackParamList, 'Auth'>) => {
+  const [region, setRegion] = useState('');
   return (<View style={styles.container}>
     <Text style={styles.thankYou}>
       Thanks for joining our ever-growing community!
@@ -16,13 +14,26 @@ const ProfileSetup = () => {
     <Text style={styles.tell}>
       Tell us a little more about yourself...
     </Text>
-    <Text>
+    <Text style={styles.regionText}>
       What region are you in?
     </Text>
-
-    <Text>
+    <RNPickerSelect
+            onValueChange={(value) => setRegion(value)}
+            items={[
+                { label: 'North America', value: 'north america' },
+                { label: 'South America & The Caribbean', value: 'south america' },
+                { label: 'Asia', value: 'Asia' },
+                { label: 'Africa', value: 'Afria' },
+                { label: 'Europe', value: 'Europe' },
+                { label: 'Australia', value: 'Australia' }
+            ]}
+        />
+    <Text style={styles.interest}>
       Which of these topics interest you?
     </Text>
+    <TouchableOpacity style = {styles.button} onPress={() => navigation.replace('Root')}>
+          <Text style = {styles.buttonText}>Ready!</Text>
+        </TouchableOpacity>
   </View>);
 };
 
@@ -36,9 +47,10 @@ const styles = StyleSheet.create({
     height: 30,
     width: 260,
     alignItems: 'center',
-    backgroundColor: 'white',
+    backgroundColor: '#1742eb',
     borderRadius: 50,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    marginTop: 60
   },
   item: {
     padding: 20,
@@ -60,7 +72,18 @@ const styles = StyleSheet.create({
   },
   register:{
     fontWeight:'bold',
-  }
+  },
+  interest:{
+    marginTop: 20
+  },
+  buttonText:{
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold'
+},
+regionText:{
+  fontWeight: 'bold'
+}
 });
 
 export default ProfileSetup;
