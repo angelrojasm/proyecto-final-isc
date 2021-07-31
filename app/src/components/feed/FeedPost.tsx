268562cca819d1f5e9b96cca17cddcdc1f05b0bf
angelrojasm <angelrojasm6@gmail.com>
2021-07-31 18:24:24 -0400

    Fixed Feed Post component reply prompt

--- app/src/components/feed/FeedPost.tsx
+++ app/src/components/feed/FeedPost.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import tailwind from 'tailwind-rn';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, Pressable } from 'react-native';
 import moment from 'moment';
 import { useNavigation } from '@react-navigation/native';
 
@@ -23,7 +23,7 @@ interface IFeedPostProps {
 const FeedPost = ({ post, refreshPosts }: IFeedPostProps) => {
   const navigation = useNavigation();
   return (
-    <View
+    <Pressable
       style={{
         ...tailwind('bg-white flex w-11/12 my-2'),
         shadowColor: '#000',
@@ -35,6 +35,9 @@ const FeedPost = ({ post, refreshPosts }: IFeedPostProps) => {
         shadowRadius: 3.84,
 
         elevation: 5,
+      }}
+      onPress={() => {
+        navigation.navigate('Replies', { post, refreshPosts });
       }}>
       <View style={tailwind('flex flex-row items-center')}>
         <Text style={tailwind('text-sm font-bold text-black my-1 ml-2')}>{post.postedBy}</Text>
@@ -43,22 +46,11 @@ const FeedPost = ({ post, refreshPosts }: IFeedPostProps) => {
         </Text>
       </View>
       <Text style={tailwind('text-sm font-light text-black ml-2 my-2')}>{post.content}</Text>
-      <Text style={tailwind('text-xs text-black font-bold ml-6')}>
-        {post.comments.length}
+      <Text style={tailwind('text-xs text-black font-bold ml-6 my-2')}>
+        {post.comments.length === 0 ? 'No' : post.comments.length}
         {post.comments.length === 1 ? ' Reply' : ' Replies'}
       </Text>
-      <View style={{ ...tailwind('flex items-center'), width: '100%' }}>
-        <TouchableOpacity
-          style={tailwind(
-            'bg-transparent border border-blue-500 rounded-md flex items-center w-1/3 my-3 px-4 py-2'
-          )}
-          onPress={() => {
-            navigation.navigate('Replies', { post, refreshPosts });
-          }}>
-          <Text style={tailwind('text-blue-400')}>Reply</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    </Pressable>
   );
 };
 
